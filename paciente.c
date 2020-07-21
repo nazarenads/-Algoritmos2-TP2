9702a7b8a747970a178d516b9a08842979d7317b
Nazarena Dos Santos <nazarena.dossantos@intive.com>
2020-07-21 20:25:13 -0300

    added calcular_anio

--- paciente.c
+++ paciente.c
@@ -1,10 +1,22 @@
 #include "paciente.h"
+#include <stdio.h>
+#include <time.h>
 #define ANIO_ACTUAL 2020
 
 struct paciente{
     char* nombre;
     size_t antiguedad;
 };
+
+int calcular_anio_actual(){
+    time_t rawtime;
+    struct tm * timeinfo;
+
+    time ( &rawtime );
+    timeinfo = localtime ( &rawtime );
+
+    return timeinfo->tm_year + 1900;
+}
 
 paciente_t* paciente_crear(char* nombre, void* antiguedad){
     paciente_t* paciente = malloc(sizeof(paciente_t));
