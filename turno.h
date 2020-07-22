c807a68ddf1634c27452052d557bfe73edad4668
Erne <ernestodainesiperrot@gmail.com>
2020-07-22 19:50:55 -0300

    added pre and post

--- turno.h
+++ turno.h
@@ -24,20 +24,24 @@ size_t ver_cant_espera_reg(turno_t* turno);
 size_t ver_cant_espera_urg(turno_t* turno);
 
 // Agrega un paciente a la lista de espera de turnos urgentes
+// Pre: el turno fue creado.
 // Post: devolvio true si se pudo encolar al paciente. False en caso de error.
 bool encolar_turno_urg(turno_t* turno, paciente_t* paciente);
 
 // Agrega un paciente a la lista de espera de turnos regulares
+// Pre: el turno fue creado.
 // Post: devolvio true si se pudo encolar al paciente. False en caso de error.
 bool encolar_turno_reg(turno_t* turno, paciente_t* paciente);
 
 // Saca un paciente de la lista de espera de turnos regulares
-// Post: devolvio true si se pudo desencolar al paciente. False en caso de error.
-bool desencolar_turno_reg(turno_t* turno);
+// Pre: el turno fue creado.
+// Post: el paciente ya no se encuentra dentro del turno.
+paciente_t* desencolar_turno_reg(turno_t* turno);
 
 // Saca un paciente de la lista de espera de turnos de urgencia
-// Post: devolvio true si se pudo desencolar al paciente. False en caso de error.
-bool desencolar_turno_urg(turno_t* turno);
+// Pre: el turno fue creado.
+// Post: el paciente ya no se encuentra dentro del turno.
+paciente_t* desencolar_turno_urg(turno_t* turno);
 
 // Destruye el turno
 void turno_destruir(turno_t* turno);
