473c03a2cd6ee17ebcb89b4add596e27799dafe2
Nazarena Dos Santos <nazarena.dossantos@intive.com>
2020-07-24 20:57:12 -0300

    added paciente_hash_crear

--- paciente.c
+++ paciente.c
@@ -1,4 +1,6 @@
 #include "paciente.h"
+#include "hash.h"
+#include "lista.h"
 #include <stdio.h>
 #include <time.h>
 
@@ -36,4 +38,28 @@ size_t paciente_antiguedad(paciente_t* paciente){
 
 void paciente_destruir(paciente_t* paciente){
     free(paciente);
+}
+
+bool guardar_paciente_en_hash(hash_t* hash, paciente_t* paciente){
+    char* nombre = paciente_nombre(paciente);
+    size_t antiguedad = paciente_antiguedad(paciente);
+    bool guardado = hash_guardar(hash, nombre, (void*)antiguedad);
+    if (!guardado) return false;
+    return true;
+}
+
+
+hash_t* paciente_hash_crear(lista_t* lista_pacientes){
+    hash_t* hash_pacientes = hash_crear(NULL);
+    if (!hash_pacientes) return NULL;
+    lista_iter_t* iter = lista_iter_crear(lista_pacientes);
+    while (!lista_iter_al_final(iter)){
+        paciente_t* paciente = (paciente_t*)lista_iter_ver_actual(iter);
+        bool guardado = guardar_paciente_en_hash(hash_pacientes, paciente);
+        if (!guardado) return NULL;
+        lista_iter_avanzar(iter);
+    }
+    lista_iter_destruir(iter);
+    lista_destruir(lista_pacientes, free);
+    return hash_pacientes;
 }
