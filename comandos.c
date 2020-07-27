7167be11444f3b8f90d55711e6aca8a4b2181dcc
Erne <ernestodainesiperrot@gmail.com>
2020-07-26 21:16:46 -0300

    added pedir_turno

---
+++ comandos.c
@@ -0,0 +1,63 @@
+#include "turno.h"
+#include "paciente.h"
+#include "doctor.h"
+#include "clinica.h"
+#include "mensajes.h"
+#include <string.h>
+#include <stdio.h>
+#define TURNO_URG "URGENTE"
+#define TURNO_REG "REGULAR"
+
+
+/* PEDIR TURNO */
+
+void pedir_turno(char** parametros, clinica_t* clinica){
+    if(strcmp(parametros[2], TURNO_URG) == 0){
+        if( pedir_turno_urgente(parametros, clinica) ){
+            // Imprimir salida de de encolado con exito
+        }
+    }else if(strcmp(parametros[2], TURNO_REG) == 0){
+        if( pedir_turno_regular(parametros, clinica) ){
+            // Imprimir salida de de encolado con exito
+        }
+    }else{
+        // Lanzar error "grado de urgencia no identificado ('parametros[2]')"
+    }
+}
+
+bool pedir_turno_urgente(char** parametros, clinica_t* clinica){
+    hash_t* hash_pacientes = clinica_ver_pacientes(clinica);
+    hash_t* hash_turnos = clinica_ver_turnos(clinica);
+    if(!hash_pertenece(hash_pacientes, parametros[0])){
+        // lanzar errror no existe le paciente 'NOMBRE_PACIENTE'
+        return false;
+    }
+    if(!hash_pertenece(hash_turnos, parametros[1])){
+        // lanzar error no existe la especialidad 'NOMBRE_ESPECIALIDAD'
+        return false;
+    }
+    turno_t* turno_urgente = hash_obtener(hash_turnos, parametros[1]);
+    paciente_t* paciente = hash_obtener(hash_pacientes, parametros[0]);
+    return encolar_turno_urg(turno_urgente, paciente);
+}
+
+bool pedir_turno_regular(char** parametros, clinica_t* clinica){
+    hash_t* hash_pacientes = clinica_ver_pacientes(clinica);
+    hash_t* hash_turnos = clinica_ver_turnos(clinica);
+    if(!hash_pertenece(hash_pacientes, parametros[0])){
+        // lanzar errror no existe le paciente 'NOMBRE_PACIENTE'
+        return false;
+    }
+    if(!hash_pertenece(hash_turnos, parametros[1])){
+        // lanzar error no existe la especialidad 'NOMBRE_ESPECIALIDAD'
+        return false;
+    }
+    turno_t* turno_regular = hash_obtener(hash_turnos, parametros[1]);
+    paciente_t* paciente = hash_obtener(hash_pacientes, parametros[0]);
+    return encolar_turno_reg(turno_regular, paciente);
+}
+
+
+
+
+
