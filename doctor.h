d76ce5dfa7a829a3d80d53273292a23be0e7b6d5
Erne <ernestodainesiperrot@gmail.com>
2020-08-04 17:24:37 -0300

    added pre & post conditions

--- doctor.h
+++ doctor.h
@@ -16,12 +16,18 @@ typedef struct doctor doctor_t;
 doctor_t* doctor_crear(char* nombre, char* especialidad);
 
 // Devuelve la especialidad del doctor
+// Pre: se creo el doctor
+// Post: se devolvio la especialidad del doctor
 char* doctor_ver_especialidad(doctor_t* doctor);
 
 // Devuelve el nombre del doctor
+// Pre: el doctor fue creado
+// Post: se devolvio el nombre del doctor
 char* doctor_ver_nombre(doctor_t* doctor);
 
 // Devuelve la cantidad de pacientes que atendio el doctor
+// Pre: el doctor fue creado
+// Post: se devolvio la cantiad de pacientes que atendio el doctor
 size_t doctor_cant_atendidos(doctor_t* doctor);
 
 //Destruye al doctor
@@ -30,4 +36,7 @@ void doctor_destruir(doctor_t* doctor);
 // Recibe el vector de campos del csv y crea un doctor_t usando esos campos
 void* crear_doctor_desde_csv(char** campos, void* extra);
 
+// Aumenta la cantidad de pacientes que atendio un doctor
+// Pre: el doctor fue creado
+// Post: se aumento la cantidad de pacientes atendidos del doctor
 void doctor_aumentar_cant_atendidos(doctor_t* doctor);
