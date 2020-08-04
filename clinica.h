fca816d7a4bc57c63cc5f8b8444288aa0b5aadd9
Erne <ernestodainesiperrot@gmail.com>
2020-08-04 17:28:33 -0300

    added pre & post conditions

--- clinica.h
+++ clinica.h
@@ -16,14 +16,20 @@ typedef struct clinica clinica_t;
 // Crea la clinica inicializando todos sus miembros
 clinica_t* clinica_crear(char* csv_pacientes, char* csv_doctores);
 
-// Destruye la clinica con todos sus miembros
-void clinica_destruir(clinica_t* clinica);
-
 // Devuelve el hash de pacientes de la clinica
+// Pre: se creo la clinica
+// Post: se devolvio el hash de pacientes de la clinica
 hash_t* clinica_ver_pacientes(clinica_t* clinica);
 
 // Devuelve el hash de turnos de la clinica
+// Pre: se creo la clinica
+// Post: se devolvio el hash de turnos de la clinica
 hash_t* clinica_ver_turnos(clinica_t* clinica);
 
 // Devuelve el abb de doctores de la clinica
+// Pre: se creo la clinica
+// Post: se devolvio el abb doctores
 abb_t* clinica_ver_doctores(clinica_t* clinica);
+
+// Destruye la clinica con todos sus miembros
+void clinica_destruir(clinica_t* clinica);
